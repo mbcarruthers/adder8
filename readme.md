02983ff84a385970433ad023c06a7b573edf43df
mbcarruthers <mbcarruthers@crimson.ua.edu>
2025-08-03 11:48:10 -0400

    removed unecessary code, cleaned readme

--- readme.md
+++ readme.md
@@ -1,8 +1,6 @@
 # Adder 8
 
-Extrapolation of a diagram from  'Digital Design from Scratch Volume 1', page 77 
-
- - Just a 3 bit addition, using only combinational logic, driven by a clock, with a testbench viewable from Surfer.
+Extrapolation of a 3 bit addition combinational logic diagram from  'Digital Design from Scratch Volume 1', page 77 
 
 !['image of adder8'](./adder8.png)
 
--- src/adder8.vhd
+++ src/adder8.vhd
@@ -25,48 +25,6 @@ begin
 end architecture;
 
 library IEEE;
-use IEEE.numeric_std.all;
-use IEEE.std_logic_1164.all;
-
-entity adder4 is
-    Port(
-        Clk : in std_logic
-        -- Clear : in std_logic
-    );
-end entity;
-
-architecture RTL of adder4 is
-    signal Clear : std_logic := '0'; 
-    signal N : std_logic_vector(3 downto 0); 
-begin
-    Clear <= '1' when (N(0) and N(1) and N(2) and N(3)) else '0';
-    b0 : entity work.d_flop
-        Port map(
-            D      => (not N(0)) and not Clear,
-            Clk    => Clk,
-            Output => N(0)
-        );
-    b1 : entity work.d_flop
-        Port map(
-            D      => (N(0) xor N(1)) and not Clear,
-            Clk    => Clk,
-            Output => N(1)
-        );
-    b2 : entity work.d_flop
-        Port map(
-            D      => ((N(0) and N(1)) xor N(2)) and not Clear,
-            Clk    => Clk,
-            Output => N(2)
-        );
-    b3 : entity work.d_flop
-        Port map(
-            D      => ((N(0) and N(1) and N(2)) xor N(3)) and not Clear,
-            Clk    => Clk,
-            Output => N(3)
-        );
-end architecture;
-
-library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
